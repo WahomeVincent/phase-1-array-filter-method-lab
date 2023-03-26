67c55889845f75e7f10da59e826089691584ffe3
WahomeVincentMwangiclear <vinwahome@gmail.com>
2023-03-26 20:17:17 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -7,3 +7,7 @@ function fuzzyMatch(drivers, str) {
     return drivers.filter(driver => driver.startsWith(str));
   }
   
+  function matchName(drivers, str) {
+    return drivers.filter(driver => driver.name === str);
+  }
+  
