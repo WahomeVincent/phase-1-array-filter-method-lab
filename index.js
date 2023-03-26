352caf22b2e27d8eda7d2032a5f822116ae6f80c
WahomeVincentMwangiclear <vinwahome@gmail.com>
2023-03-26 20:15:54 +0300

    6 passing/ 1 failing

--- index.js
+++ index.js
@@ -3,3 +3,7 @@ function findMatching(drivers, str) {
     return drivers.filter(driver => driver.toLowerCase() === str.toLowerCase());
   }
   
+function fuzzyMatch(drivers, str) {
+    return drivers.filter(driver => driver.startsWith(str));
+  }
+  
